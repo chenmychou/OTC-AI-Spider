396cd109ba8b9fbd5b7e28d5cdbd03d48de2a4d5
dongchongyang <dongchongyang@meituan.com>
2017-12-22 11:06:32 +0800

    feat: first commit

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const moment = require('moment')
 
 const OTC = `https://otcbtc.com/sell_offers?utf8=%E2%9C%93&currency=eos&fiat_currency=cny&amount=&payment_type=all&commit=%E6%90%9C%E7%B4%A2%E5%85%91%E6%8D%A2`
 
-const selector = 'ul.list-content .price'
-
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 const fetchEosPrice = async () => {
